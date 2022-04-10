5af4e0b063a8cfeac169ce2c953941c585e48731
Sainikhil Reddy <sainikhilvatti1605@gmail.com>
2022-04-10 19:06:19 +0530

    Disabled eslint for prod

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ["i.ytimg.com", "yt3.ggpht.com"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
